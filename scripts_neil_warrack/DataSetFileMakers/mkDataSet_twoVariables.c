656d45d934ada93373011d46a933ca383d98da3d
neiltheory <thewazzak@hotmail.com>
2016-06-29 14:04:44 +0100

    usable script to compile two branches of a TTree into one ROOT file

--- scripts_neil_warrack/DataSetFileMakers/mkDataSet_twoVariables.c
+++ scripts_neil_warrack/DataSetFileMakers/mkDataSet_twoVariables.c
@@ -1,4 +1,3 @@
-// From Gedeminas work
 #ifndef __CINT__
 #include "RooGlobalFunc.h"
 #endif
@@ -12,19 +11,23 @@
 using namespace RooFit ;
 
 
+// Script will write .root dataset (with two variables) file from TTree 
+
 void mkfunc() {
-  // Write .root dataset file from TTree 
+
 
   TFile *file_01 = TFile::Open("~/Documents/uni/LHCb_CharmSummerProj/data/turbo_2015_data.root");
   //TFile *file_01 = TFile::Open("/afs/phas.gla.ac.uk/user/n/nwarrack/public_ppe/myLHCb/Gedcode/LHCb_CharmedHadrons/data/turbo_2015_data.root") ;
 
   TTree* mytree = (TTree*) file_01->Get("Lambda_cToKppiTuple/DecayTree;1");
   RooRealVar Lambda_cplus_TAU("Lambda_cplus_TAU","Lambda_cplus_TAU",0.0002,0.0022);
-  RooDataSet ds("ds","ds",RooArgSet(Lambda_cplus_TAU),Import(*mytree)) ;
+ 
+ RooRealVar Lambda_cplus_M("Lambda_cplus_M","Lambda_cplus_M",2216,2356) ;
+ RooDataSet ds("ds","ds",RooArgSet(Lambda_cplus_TAU, Lambda_cplus_M),Import(*mytree)) ;
   // Choose destination, and name appropriately.
   //TFile *datafile = TFile::Open("~/Documents/uni/LHCb_CharmSummerProj/learning_root/RooFit/firstTry/datafileLambda_TAUminNeg002_max005.root","recreate");
   //TFile *datafile = TFile::Open("/afs/phas.gla.ac.uk/user/n/nwarrack/public_ppe/myLHCb/Gedcode/LHCb_CharmedHadrons/fircode_neil_warrack/outputfile.root") ;
-  TFile *datafile = TFile::Open("~/Documents/uni/LHCb_CharmSummerProj/Gedcode/baryon-lifetimes-2015/data/run-II-data/datafileLambda_TAUmin200fs_max2200fs.root","recreate");
+  TFile *datafile = TFile::Open("~/Documents/uni/LHCb_CharmSummerProj/Gedcode/baryon-lifetimes-2015/data/run-II-data/datafileLambda_TAUmin200fs_max2200fs_Mmin2216_max2356.root","recreate");
   ds.Write() ;
   datafile->Close(); 
  
