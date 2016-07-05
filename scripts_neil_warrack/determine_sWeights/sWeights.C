3bf57e534909f3e681ef9a84af69053d17b295ed
neiltheory <thewazzak@hotmail.com>
2016-07-05 17:31:58 +0100

    working script to find sWeights from a ROOT file

--- scripts_neil_warrack/determine_sWeights/sWeights.C
+++ scripts_neil_warrack/determine_sWeights/sWeights.C
@@ -22,18 +22,21 @@
 using namespace RooFit ;
 
 
-// Script opens a previously made .root file which contains a dataset type object and calculates 'sWeights'.
+// Script opens a previously made ROOT file which contains a dataset type object and calculates 'sWeights'.
+// sWeights are then saved to a dataset type ROOT file AND also to a TTree type ROOT file, along with their
+// original info.
+
 
 void sWeigher() {
 
   TFile *fullFile = TFile::Open("~/Documents/uni/LHCb_CharmSummerProj/learning_root/turbo_2015_data.root") ;
-
   TTree* mytree = (TTree*) fullFile->Get("Lambda_cToKppiTuple/DecayTree;1") ;  
-
   TFile *datafile = TFile::Open("~/Documents/uni/LHCb_CharmSummerProj/Gedcode/baryon-lifetimes-2015/data/run-II-data/datafileLambda_TAUmin200fs_max2200fs_Mmin2216_max2356_CutIPCHI2lt3.root") ; 
+
 
   // Define dataset
   RooDataSet* ds = (RooDataSet*)datafile->Get("ds") ;
+
 
   // Define TAU variable, get limits.
   RooRealVar Lambda_cplus_TAU("Lambda_cplus_TAU","Lambda_cplus_TAU",0.0002 ,0.0022 ,"ns") ;  //real range of interest is [0.00025, 0.002], this is defined later.
@@ -50,9 +53,9 @@ void sWeigher() {
   // Define IPCHI2 variable
   RooRealVar Lambda_cplus_IPCHI2_OWNPV("Lambda_cplus_IPCHI2_OWNPV","Lambda_cplus_IPCHI2_OWNPV",-100 ,100) ; 
 
-  // Build combined double Gaussian PDF; "gaussComb".
- 
-  double mass_peak = 2286 ;  // initialize gaussian mean value for fit
+
+  // Build combined double Gaussian PDF; "gaussComb". 
+  double mass_peak = 2286 ;  // initialize gaussian mean value for fit for Lambda_cplus
 
   RooRealVar gausMean1("gausMean1", "gausMean1",mass_peak, lowestM, highestM, "GeV") ;
   RooRealVar sigma1("sigma1","sigma1", 6, 0, 50) ;
@@ -71,6 +74,7 @@ void sWeigher() {
   RooRealVar expoPar("expoPar","expoPar", -0.0001, -1., 0.);
   RooExponential expo_bkg("expo_bkg", "expo_bkg", Lambda_cplus_M, expoPar);
 
+
   // Build model PDF
   RooRealVar nSignal("nSignal","nSignal", 200000, 0, 409570);
   RooRealVar nBkg("nBkg","nBkg", 200000, 0, 409570);
@@ -79,10 +83,12 @@ void sWeigher() {
   //RooAddPdf model("model","model",RooArgList(gauss, expo_bkg),RooArgList(nSignal, nBkg));
   RooAddPdf model("model","model",RooArgList(gaussComb, expo_bkg),RooArgList(nSignal, nBkg));
 
+
   // Fit model
   model.fitTo(*ds, Extended()) ;
 
-  // Set model fit variables to constants (NOT COEFFs!)
+
+  // Set model fit variables to constants (NOT COEFFs!) ; this is important for the sPlot function below.
   gausMean1.setConstant() ;
   gausMean2.setConstant() ;
   sigma1.setConstant() ;
@@ -95,40 +101,40 @@ void sWeigher() {
   // Create and record sWeights
   RooStats::SPlot* sDataM = new RooStats::SPlot("sData", "An SPlot", *ds, &model, RooArgList(nSignal, nBkg)) ;
 
+  // Check the values of the yields and the 'yields' from the sWeight are the same
   std::cout << std::endl <<  "Yield of signal is " << nSignal.getVal() << ".  From sWeights it is " << sDataM->GetYieldFromSWeight("nSignal") << std::endl;
   std::cout << "Yield of background is " << nBkg.getVal() << ".  From sWeights it is " << sDataM->GetYieldFromSWeight("nBkg") << std::endl << std::endl;
 
+ /*
   for (Int_t i=90; i < 100; i++){
     cout << "nSignal: "<<sDataM->GetSWeight(i,"nSignal")
 	 << ", nBkg: "<<sDataM->GetSWeight(i,"nBkg")
-	 << ", Total Weight: "<<sDataM->GetSumOfEventSWeight(i)
+	 << ", Total Weight: "<<sDataM->GetSumOfEventSWeight(i) // Should be total = 1 (always!)
 	 << endl;
   }
+  */
 
-  // Code for saving dataset with weights
-  //RooRealVar w("w","w",-5000,5000);
-  //RooDataSet Wgt("Wgt","Wgt", mytree, RooArgSet(Lambda_cplus_M), WeightVar(w));
-  TFile *dsWithWeights = TFile::Open("datafileLambda_TAUmin200fs_max2200fs_Mmin2216_max2356_CutIPCHI2lt3_withWeights.root","RECREATE") ;
+
+  // Save dataset with sWeights
+   TFile *dsWithWeights = TFile::Open("datafileLambda_TAUmin200fs_max2200fs_Mmin2216_max2356_CutIPCHI2lt3_withWeights.root","RECREATE") ;
   ds->Write() ;
   dsWithWeights->Close() ;
-  cout<<endl<<"datafileLambda_TAUmin200fs_max2200fs_Mmin2216_max2356_CutIPCHI2lt3_withWeights.root created in current directory..."<<endl ;
+  cout<<endl<<"  Dataset type file (datafileLambda_TAUmin200fs_max2200fs_Mmin2216_max2356_CutIPCHI2lt3_withWeights.root) created in current directory..."<<endl ;
  
-  cout<<"plotting signal and background..."<<endl;
-
+  // Save TTree with sWeights
   TFile treefile("sTree.root", "recreate") ;
-  cout<<"step"<<endl;  
-  RooTreeDataStore sTree("sTree", "sTree", *ds->get(0), *ds->store()) ;
-  cout<<"step"<<endl;    
-  TTree* tree = sTree.tree() ;
-  cout<<"step"<<endl;  
-  tree->SetBranchStatus("Lambda_cplus_M", 0) ; // you don't want to save the mass variables again.
-  cout<<"step"<<endl;  
-  tree->Write() ;
-  cout<<"step"<<endl;  
+   RooTreeDataStore sTree("sTree", "sTree", *ds->get(0), *ds->store()) ;
+  cout<<"test1"<<endl;    
+  TTree& mystree = sTree.tree() ;
+  mystree.SetBranchStatus("Lambda_cplus_M", 0) ; // you don't want to save the mass variables again.
+  mystree.Write() ;
   treefile.Close() ;
-  cout<<"step"<<endl;  
+  cout<<endl<<"  TTree file (sTree.root) created in current directory..."<<endl ;
 
 
+
+  // VISUAL CHECK: check the fit is correct, plot the fitted model on the data
+  /*
   // Plot =================================================
   RooPlot *fullDataFit = Lambda_cplus_M.frame(Title("SignalAndBkg"));
    ds->plotOn(fullDataFit, Name("data"), MarkerColor(kBlack)) ;
@@ -149,24 +155,6 @@ void sWeigher() {
   c102->Update() ;  
   c102->Print("SignalAndBkg01.pdf");
   // ======================================================
+  */
 }
 
-// Canvas formatting (4 plots on landscape A4)
-void formatCanvas4(TCanvas* canvas){
-  canvas->SetBorderSize(2);
-  canvas->SetBorderMode(-1);
-  canvas->Clear();
-  canvas->Divide(2,2);
-  canvas->cd(1);
-  gPad->SetLeftMargin(0.2);
-  gPad->SetBottomMargin(0.2);
-  canvas->cd(2);
-  gPad->SetLeftMargin(0.2);
-  gPad->SetBottomMargin(0.2);
-  canvas->cd(3);
-  gPad->SetLeftMargin(0.2);
-  gPad->SetBottomMargin(0.2);
-  canvas->cd(4);
-  gPad->SetLeftMargin(0.2);
-  gPad->SetBottomMargin(0.2);
-}
